0071469b4cd8c6ca8fddda16fc0101d4640dd569
Ryo Somatani <ryo_somatani@outlook.com>
2022-09-28 00:15:13 -0700

    lab-3: add delete functionality of posts

--- lab-3/main.js
+++ lab-3/main.js
@@ -3,6 +3,7 @@ const postTemplate = document.querySelector('#single-post');
 const form = document.querySelector('#new-post form');
 const fetchButton = document.querySelector('#available-posts button');
 const postList = document.querySelector('ul');
+let currentData;
 
 function sendHttpRequest(method, url, data) {
   // with XHR
@@ -52,18 +53,48 @@ async function fetchPosts() {
     'https://jsonplaceholder.typicode.com/posts'
   )
     .then(({ data }) => {
-      for (const post of data) {
-        const postElClone = document.importNode(postTemplate.content, true);
-        postElClone.querySelector('h2').textContent = post.title.toUpperCase();
-        postElClone.querySelector('p').textContent = post.body;
-        postElClone.querySelector('li').id = post.id;
-        listElement.appendChild(postElClone);
-      }
+      currentData = data;
+      createPostItems();
     })
     .catch((err) => {
       console.error('Error Message => ', err);
     });
 }
 
+async function deletePost(id) {
+  sendHttpRequest(
+    'DELETE',
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  ).then((res) => {
+    console.log(`Deleted post with id of ${id}`);
+    currentData = currentData.filter((item) => item.id !== parseInt(id));
+    document.querySelector('.posts').innerHTML = '';
+    createPostItems();
+  });
+}
+
+function createPostItems() {
+  for (const post of currentData) {
+    const postElClone = document.importNode(postTemplate.content, true);
+    postElClone.querySelector('h2').textContent = post.title.toUpperCase();
+    postElClone.querySelector('p').textContent = post.body;
+    postElClone.querySelector('li').id = post.id;
+    listElement.appendChild(postElClone);
+  }
+
+  // DELETE
+  document.querySelectorAll('.posts button').forEach((button) => {
+    button.addEventListener('click', (e) => {
+      const id = e.target.closest('.post-item').id;
+      deletePost(id);
+    });
+  });
+}
+
 //READ
 fetchButton.addEventListener('click', fetchPosts);
+
+//Delete
+deleteButtons.forEach((button) => {
+  button.addEventListener('click', (e) => deletePost(e));
+});
